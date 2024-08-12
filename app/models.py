aa2dd5455a06f8cdd237dd396e936818cbedc8a9
Zain_ibrahem1 <85264650+zain-a234@users.noreply.github.com>
2024-08-12 03:54:03 -0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,15 +3,15 @@ from django.db import models
 from django.core.validators import EmailValidator
 # Create your models here.
 
-class Professor(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)#
-    email = models.EmailField(max_length=254, validators=[EmailValidator()])#
-    p = models.BooleanField()#
+#######################################################################
+class Room(models.Model):
+    name = models.CharField(max_length=10)
+    number_of_student=models.IntegerField()##
+    lab = models.BooleanField()
+    size = models.IntegerField()
 
     def __str__(self):
-        return self.first_name ,self.last_name , self.email , self.p
-
+        return self.name , self.number_of_student ,self.size
 
 
 ##########################################################################
@@ -23,10 +23,23 @@ class Course(models.Model):
     Mr = models.CharField(max_length=100)
     duration = models.IntegerField()
 
-
-
     def __str__(self):
         return self.name
+
+
+
+
+#########################################################################
+class Professor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)#
+    email = models.EmailField(max_length=254, validators=[EmailValidator()])#
+    email_second =models.EmailField(max_length=254, validators=[EmailValidator()])
+    password = models.IntegerField()
+    p = models.BooleanField()#
+
+    def __str__(self):
+        return self.first_name ,self.last_name , self.email , self.p
 
 
 
@@ -43,29 +56,37 @@ class Year_student(models.Model):
     ]
     name_collage = models.CharField(max_length=10)
     current_year=models.IntegerField()##
-    year_of_student=models.IntegerField()
-    number_of_days = models.IntegerField()##
-    number_of_first_session_student= models.IntegerField()##
-    number_of_first_session_category=models.IntegerField()##
-    number_of_second_session_student= models.IntegerField()##
-    number_of_second_session_category=models.IntegerField()##    
+    number_of_day=models.IntegerField()
+    number_of_student_of_first_year_first_session = models.IntegerField()##
+    number_of_student_of_first_year_second_session= models.IntegerField()##
+    number_of_category_of_first_year_first_session=models.IntegerField()##
+    number_of_category_of_first_year_second_session= models.IntegerField()##
+
+    number_of_student_of_second_year_first_session = models.IntegerField()##
+    number_of_student_of_second_year_second_session= models.IntegerField()##
+    number_of_category_of_second_year_first_session=models.IntegerField()##
+    number_of_category_of_second_year_second_session= models.IntegerField()##
+
+    number_of_student_of_third_year_first_session = models.IntegerField()##
+    number_of_student_of_third_year_second_session= models.IntegerField()##
+    number_of_category_of_third_year_first_session=models.IntegerField()##
+    number_of_category_of_third_year_second_session= models.IntegerField()##
+
+    number_of_student_of_fourth_year_first_session = models.IntegerField()##
+    number_of_student_of_fourth_year_second_session= models.IntegerField()##
+    number_of_category_of_fourth_year_first_session=models.IntegerField()##
+    number_of_category_of_fourth_year_second_session= models.IntegerField()##
+
+    number_of_student_of_fith_year_first_session = models.IntegerField()##
+    number_of_student_of_fith_year_second_session= models.IntegerField()##
+    number_of_category_of_fith_year_first_session=models.IntegerField()##
+    number_of_category_of_fith_year_second_session= models.IntegerField()##
+    number_of_day=models.IntegerField()##    
     holiday = models.CharField(max_length=10, choices=DAYS_OF_WEEK, null=True, blank=True) 
     
     def __str__(self):
         return self.name , self.number_of_student ,self.size
 
-
-
-
-#######################################################################
-class Room(models.Model):
-    name = models.CharField(max_length=10)
-    number_of_student=models.IntegerField()##
-    lab = models.BooleanField()
-    size = models.IntegerField()
-
-    def __str__(self):
-        return self.name , self.number_of_student ,self.size
 
 
 #######################################################################
@@ -84,7 +105,6 @@ class Class(models.Model):
     seesion_on_years= [
         ('first', 'first'),
         ('first', 'first'),
- 
     ]
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
